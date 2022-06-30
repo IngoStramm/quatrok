282b1d8f56ee7ff3a5006ce4b0b5d08f2f209cfd
IngoStramm <ingostramm@gmail.com>
2022-06-30 17:57:19 -0300

    add: elementor btn class

--- quatrok.php
+++ quatrok.php
@@ -4,7 +4,7 @@
  * Plugin Name: Quatro K
  * Plugin URI: https://agencialaf.com
  * Description: Plugin para o site do Quatro K.
- * Version: 0.0.1
+ * Version: 0.0.2
  * Author: Ingo Stramm
  * Text Domain: qk
  * License: GPLv2
