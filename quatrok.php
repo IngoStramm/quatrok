2bd15d86c261eb27809607b094f23a21c7ed4a9b
Ingo Stramm <ingostramm@gmail.com>
2022-07-28 09:14:23 -0300

    fix: calc shortcode btn style

--- quatrok.php
+++ quatrok.php
@@ -4,7 +4,7 @@
  * Plugin Name: Quatro K
  * Plugin URI: https://agencialaf.com
  * Description: Plugin para o site do Quatro K.
- * Version: 0.0.6
+ * Version: 0.0.7
  * Author: Ingo Stramm
  * Text Domain: qk
  * License: GPLv2
