1b61a1d0fcfa235b4793d1e2759d8e853fd5fba9
Ingo Stramm <ingostramm@gmail.com>
2024-03-28 08:27:23 -0300

    add minor css change

--- quatrok.php
+++ quatrok.php
@@ -4,7 +4,7 @@
  * Plugin Name: Quatro K
  * Plugin URI: https://agencialaf.com
  * Description: Plugin para o site do Quatro K.
- * Version: 0.0.12
+ * Version: 0.0.13
  * Author: Ingo Stramm
  * Text Domain: qk
  * License: GPLv2
